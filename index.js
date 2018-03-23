c3e37c0110c05fa6251fdbdbd02d6944e3203a40
Prinzhorn <alexander@prinzhorn.it>
2018-03-23 15:57:26 +0100

    ffmpeg does things

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const spawn = require('child_process').spawn;
+const process = require('process');
+
+const moment = require('moment');
 const puppeteer = require('puppeteer');
 
 const url = 'http://localhost:8080/examples/';
 
-const duration = 2;
+const duration = 0.3;
 const numberOfFrames = Math.ceil(duration * 60);
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.goto(url, {
@@ -30,11 +34,14 @@ const numberOfFrames = Math.ceil(duration * 60);
   }, duration);
 
   for (let i = 0; i <= numberOfFrames; i++) {
-    let progress = i / numberOfFrames;
+    const progress = i / numberOfFrames;
+
+    console.log(`taking screenshot ${i} of ${numberOfFrames}`);
 
     await page.evaluate(progress => {
       return new Promise(resolve => {
         window.scrollCastTween.progress(progress);
+
         requestAnimationFrame(() => {
           setTimeout(resolve);
         });
@@ -45,4 +52,34 @@ const numberOfFrames = Math.ceil(duration * 60);
   }
 
   await browser.close();
+
+  const videoFileName = `Scrollmeister-scrollcast-${moment().format('YYYY-MM-DD HH-mm-ss')}.mp4`;
+  const wget = spawn('ffmpeg', [
+    '-r',
+    '60',
+    '-f',
+    'image2',
+    '-s',
+    '1280x720',
+    '-i',
+    'frames/%04d.png',
+    '-vcodec',
+    'libx264',
+    '-crf',
+    '25',
+    '-pix_fmt',
+    'yuv420p',
+    videoFileName
+  ]);
+
+  wget.stderr.pipe(process.stderr);
+  wget.stderr.pipe(process.stderr);
+
+  wget.on('close', code => {
+    if (code !== 0) {
+      return console.error(`Error, ffmpeg exited with status ${code}`);
+    }
+
+    console.log(`created ${videoFileName}`);
+  });
 })();
